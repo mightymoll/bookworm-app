e2a44b323e2c196fb65564a7dd4c357e516841ab
molly <mnfelts@gmail.com>
2025-03-20 12:40:18 +0100

    register route
    - use info from req.body to create new user if :
      - all info is complete (email, username password)
      - password & username are long enough
      - username and email are not already existing

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,45 @@
 import express from "express";
+import User from "../models/User.js"
 
 const router = express.Router();
 
 router.post("/register", async (req, res) =>{
-	res.send("register")
+	try{
+		const {username,email,password} = req.body;
+
+		// check that username, email, and password exist
+		if(!username || !email || !password){
+			return res.status(400).json({message: "All fields required"});
+		}
+		// check password & username is the minimum length
+		if(password.length < 8){
+			return res.status(400).json({message: "Password should be at least 8 characters long"});
+		}
+		if(username.length < 3){
+			return res.status(400).json({message: "Username should be at least 3 characters long"});
+		}
+
+		// check if email or username already exists
+		const existingEmail = await User.findOne({email});
+		if (existingEmail){
+			return res.status(400).json({message: "Email already exists"});
+		}
+				const existingUsername = await User.findOne({username});
+		if (existingUsername){
+			return res.status(400).json({message: "Username already exists"});
+		}
+
+		const user = new User({
+			email,
+			username,
+			password,
+			profileImage: "",
+		})
+
+	}
+	catch(error){
+
+	}
 })
 
 router.post("/login", async (req, res) =>{
