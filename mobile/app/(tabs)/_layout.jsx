6813bc5ce5a0cf79cda2df895344d8d65905b75d
molly <mnfelts@gmail.com>
2025-04-06 12:37:34 +0200

    Tab navigation layout
    - added icons and styling to tab navigation bar

--- mobile/app/(tabs)/_layout.jsx
+++ mobile/app/(tabs)/_layout.jsx
@@ -1,15 +1,68 @@
 // file to add icons and styling to all tabs
-import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import COLORS from '../../constants/colors';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-// name = filename for each screen within tab navigation
+// Tabs.Screen name = filename for each screen within tab navigation
+// Tabs screenOptions = styling for tab bar using imported colors & safe insets with useSafeAreaInsets
+
 export default function TabLayout() {
+	const insets = useSafeAreaInsets();
+
 	return (
-		<Tabs>
-			<Tabs.Screen name="index" />
-			<Tabs.Screen name="create" />
-			<Tabs.Screen name="profile" />
+		<Tabs
+		screenOptions={{
+			headerShown: false,
+			tabBarActiveTintColor: COLORS.primary,
+			headerTitleStyle:{
+				color: COLORS.textPrimary,
+				fontWeight: "600",
+			},
+			headerShadowVisible: false,
+			tabBarStyle: {
+				backgroundColor: COLORS.cardBackground,
+				borderTopWidth: 1,
+				borderTopColor: COLORS.border,
+				paddingTop: 5,
+				paddingBottom: insets.bottom,
+				height: 60 + insets.bottom,
+			},
+		}}>
+			<Tabs.Screen name="index"
+				options={{
+					title: "Home",
+					tabBarIcon:({color,size})=>(
+					<Ionicons
+						name="home-outline"
+						size={size}
+						color={color} />
+					),
+				}} 
+			/>
+			<Tabs.Screen name="profile"
+				options={{
+					title: "Profile",
+					tabBarIcon:({color,size})=>(
+					<Ionicons
+						name="person-outline"
+						size={size}
+						color={color} />
+					),
+				}} 
+			/>
+			<Tabs.Screen name="create"
+			options={{
+					title: "Create",
+					tabBarIcon:({color,size})=>(
+					<Ionicons
+						name="add-circle-outline"
+						size={size}
+						color={color} />
+					),
+				}} 
+			/>
 		</Tabs>
 	)
 }
